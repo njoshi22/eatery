eeb1d64ce38a00441a734110096b850875f6efdf
Neil <njoshi22@icloud.com>
2015-01-05 03:01:15 +0530

    working on controllers
    everything else seems to be A-OKAY except ember cli reverted back to
    0.21

--- app/controllers/restaurant/schedules.js
+++ app/controllers/restaurant/schedules.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
+  needs: ["restaurant.index"],
+  restaurant: Ember.computed.alias('controllers.restaurant.index'),
+  actions: {
+    createSchedule: function() {
+      var schedule = this.store.createRecord('schedule', {
+        time: '2100',
+        booked: false
+      });
+      var restaurant = this.get('restaurant');
+      var schedules = restaurant.get('schedules');
+      schedules.pushObject(schedule);
+      schedule.save();
+      restaurant.save();
+    }
+  }
 });
--- app/models/schedule.js
+++ app/models/schedule.js
@@ -3,7 +3,4 @@ import {Model,attr} from 'fireplace';
 export default Model.extend({
   time: attr({default: "2000"}),
   booked: attr("boolean", {default: false})
-  // isOpen: function() {
-  //     // boolean value based on whether open timings < current time < closed time
-  // },
 });
