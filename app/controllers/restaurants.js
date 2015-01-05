c086c17608c1ce267aa41235df2e6c09848a0c3e
Neil <njoshi22@icloud.com>
2015-01-06 03:20:22 +0530

    awesome app 2

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    makeBooking: function(timeslot) {
+      timeslot.set('booked',true);
+      timeslot.save();
+    }
+  }
 });
